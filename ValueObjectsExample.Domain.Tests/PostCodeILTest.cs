2b4f56cf1a1a172e5c6e50288b7f89023d3a15b4
David Mordechai <david.mordechai79@gmail.com>
2021-04-30 12:30:43 +0300

    Add project files.

---
+++ ValueObjectsExample.Domain.Tests/PostCodeILTest.cs
@@ -0,0 +1,45 @@
+using System;
+using ValueObjectsExample.Domain.ValueObjects;
+using Xunit;
+
+namespace ValueObjectsExample.Domain.Tests
+{
+    public class PostCodeILTest
+    {
+        [Fact]
+        public void PostCodeILTypeShouldExceptIntValue()
+        {
+            PostCodeIL postCode = 1234567;
+            Assert.Equal(1234567, (int)postCode);
+        }
+
+        [Theory]
+        [InlineData(-123456)]
+        public void PostCodeILShouldPositiveNumber(int code)
+        {
+            Assert.Throws<Exception>(() => (PostCodeIL)code);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(123)]
+        [InlineData(123456)]
+        [InlineData(12345678)]
+        [InlineData(-123456)]
+        public void PostCodeILShouldThrowExceptionIfPostCodeNot7DigitLong(int code)
+        {
+            Assert.Throws<Exception>(() => (PostCodeIL)code);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData(1234567, 1234567)]
+        public void SameTwoPostCodes_EquelEquel_ShouldBeTrue(int? left, int? right)
+        {
+            PostCodeIL postCodeIL_left = left;
+            PostCodeIL postCodeIL_right = right;
+            //Assert.True(postCodeIL_left == postCodeIL_right);
+            Assert.True(postCodeIL_left.Equals(postCodeIL_right));
+        }
+    }
+}
---
+++ ValueObjectsExample.Domain/ValueObjects/PostCodeIL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValueObjectsExample.Domain.ValueObjects
+{
+    public class PostCodeIL : ValueObject
+    {
+        private int Code { get; }
+        
+        private PostCodeIL(int code) 
+        {
+            if (code < 0)
+                throw new Exception("PostCodeIL must be positive number");
+
+            if (code.ToString().Length < 7 || code.ToString().Length > 7)
+                throw new Exception("Israel Postcode must be 7 digit long.");
+            Code = code;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Code;
+        }
+
+        public override bool Equals(object obj) => base.Equals(obj);
+        protected bool Equals(PostCodeIL other) => Code == other.Code;
+
+        public static bool operator ==(PostCodeIL left, PostCodeIL right)
+        {
+            if (left is null)
+                return right is null;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(PostCodeIL left, PostCodeIL right) => (left == right) is false;
+
+        public override int GetHashCode() => base.GetHashCode();
+
+        public static implicit operator PostCodeIL(int code) => new(code);
+
+        public static implicit operator int(PostCodeIL postCodeIL) => postCodeIL.Code;
+    }
+}
